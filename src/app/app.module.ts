11eb6b60975fa1a34bc5a18909a171df566c55d7
Sunny Kewat <sunnykewat99@gmail.com>
2024-09-16 11:20:27 +0530

    Components home, admin, dashboard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
